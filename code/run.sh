78cdc12e33c6854b4368a9657b7886e213ba814a
Mattias Hemmingsson <mattias.hemmingsson@booli.se>
2020-04-14 09:05:12 +0200

    Replace dblle qutas to singel

--- code/run.sh
+++ code/run.sh
@@ -46,10 +46,10 @@ echo "Exporting mongodb query"
 mkdir /opt/backup/mongo_out
 if [ $MONGO_USER = "NONE" ]
 then
-mongoexport  --db=$MONGO_DB --collection=$MONGO_COL --out=/opt/backup/mongo_out/export.json --query="$MONGO_QUERY"
+mongoexport  --db=$MONGO_DB --collection=$MONGO_COL --out=/opt/backup/mongo_out/export.json --query='$MONGO_QUERY'
 
 else
-mongoexport --host="$MONGO_HOST:27017"  -u=$MONGO_USER -p=$MONGO_PASS  --db=$MONGO_DB --collection=$MONGO_COL --authenticationDatabase admin --out=/opt/backup/mongo_out/export.json --query "$MONGO_QUERY"
+mongoexport --host="$MONGO_HOST:27017"  -u=$MONGO_USER -p=$MONGO_PASS  --db=$MONGO_DB --collection=$MONGO_COL --authenticationDatabase admin --out=/opt/backup/mongo_out/export.json --query '$MONGO_QUERY'
 echo "Your data have bean saved to /mongo_out folder"
 fi
 fi
